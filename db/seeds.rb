c2b4cca011c536749705a2c64ba1a9989d3a48c4
M Sibai <sibaim@gmail.com>
2017-03-19 13:40:01 +0100

    Artists Overview Page Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 #Artists
 taylor = Artist.create(name:"Taylor Swift", picture:"http://res.cloudinary.com/dxfy5wpqt/image/upload/v1489870469/codaisseurify/swift.jpg")
-
+carly = Artist.create(name:"Carly Rae Jepsen", picture:"http://res.cloudinary.com/dxfy5wpqt/image/upload/c_scale,h_250/v1489915388/codaisseurify/carly.jpg")
 
 
 #songs
 blank = Song.create(name:"Blank Space", artist:taylor)
-blank = Song.create(name:"Wildest Dreams", artist:taylor)
+widest = Song.create(name:"Wildest Dreams", artist:taylor)
+call = Song.create(name:"Call Me Maybe", artist:carly)
